ce67dd8ad9b44a736d2aac04dcf380568770bd4b
ttsoares <ttsoares@orion.ufrgs.br>
2022-02-27 14:09:13 -0300

    Classes - state

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM  from 'react-dom';
 //import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+        this.state = { lat: null }; // Only in the constructor !!!
 
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
             err => console.log(err)
         );
+    }
 
-        return <div>Latitude: </div>;
-    };
-};
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
